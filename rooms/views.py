f113042ae23d486b26329e11a4bf2f5fb0911622
DS Hwang <hds2g22@gmail.com>
2021-10-20 09:34:48 +0900

    Homeview: Next/Previous page navigation

--- rooms/views.py
+++ rooms/views.py
@@ -9,7 +9,7 @@ def all_rooms(request):
     page_size = 10
     limit = page_size * page
     offset = limit - page_size
-    page_count = ceil(models.Room.objects.count() / page_size) + 1
+    page_count = ceil(models.Room.objects.count() / page_size)
     all_rooms = models.Room.objects.all()[offset:limit]
 
     return render(
