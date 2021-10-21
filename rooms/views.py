9dcdeb02f13efd6858d52b4b81527e8dce95299e
DS Hwang <hds2g22@gmail.com>
2021-10-21 09:20:31 +0900

    DetailView: Using DetailView CBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,5 @@
 from django.utils import timezone
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from . import models
@@ -22,10 +22,7 @@ class HomeView(ListView):
         return context
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        # print(room.amenities.all())
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        return redirect(reverse("core:home"))
+class RoomDetail(DetailView):
+    """RoomDetail Definition"""
+
+    model = models.Room
