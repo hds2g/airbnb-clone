6740bca959aa36aabf2ccfc693c3433a3b20a949
DS Hwang <hds2g22@gmail.com>
2021-10-30 13:57:32 +0900

    SignUp

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model
-from .models import User
 from . import models
 
 
 class LoginForm(forms.Form):
 
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email"}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
 
     def clean(self):
         email = self.cleaned_data.get("email")
@@ -24,13 +24,21 @@ class LoginForm(forms.Form):
 
 
 class SignUpForm(UserCreationForm):
-
-    """class Meta:
+    class Meta:
         model = models.User
         fields = ("first_name", "last_name", "email")
+        widgets = {
+            "first_name": forms.TextInput(attrs={"placeholder": "First Name"}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Last Name"}),
+            "email": forms.EmailInput(attrs={"placeholder": "Email Name"}),
+        }
 
-    password = forms.CharField(widget=forms.PasswordInput)
-    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"})
+    )
 
     def clean_password1(self):
         password = self.cleaned_data.get("password")
@@ -40,17 +48,10 @@ class SignUpForm(UserCreationForm):
         else:
             return password
 
-    def save(self):
+    def save(self, *args, **kwargs):
         user = super().save(commit=False)
         email = self.cleaned_data.get("email")
         password = self.cleaned_data.get("password")
         user.username = email
         user.set_password(password)
         user.save()
-    """
-
-    username = forms.EmailField(label="Email")
-
-    class Meta(UserCreationForm.Meta):
-        model = User
-        # fields = ["username", "email", "first_name", "last_name"]
