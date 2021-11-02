b5e138b1a6f1577cb352e6f828b3693aa0291890
DS Hwang <hds2g22@gmail.com>
2021-11-02 09:33:07 +0900

    Room: Photo Grid

--- rooms/models.py
+++ rooms/models.py
@@ -110,3 +110,7 @@ class Room(core_models.TimeStampedModel):
     def first_photo(self):
         (photo,) = self.photos.all()[:1]  # get first array
         return photo.file.url
+
+    def get_next_four_photos(self):
+        photos = self.photos.all()[1:5]
+        return photos
